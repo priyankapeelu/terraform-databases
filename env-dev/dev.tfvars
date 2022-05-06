475b414415dfb47a5738a15e6f2a2eaf34e76f53
priyankapeelu <priyankapeelu@icloud.com>
2022-05-06 15:45:58 +0530

    terraform variablescd tt

--- env-dev/dev.tfvars
+++ env-dev/dev.tfvars
@@ -1,1 +1,2 @@
 ENV = "dev"
+WORKSTATION_IP = "172.31.80.225"
--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
 module "mongodb" {
   source                    = "./vendor/modules/mongodb"
   ENV                       = var.ENV
+  WORKSTATION_IP  =var.WORKSTATION_IP
 }
 
 module "redis" {
   source                     = "./vendor/modules/redis"
+  ENV                       = var.ENV
+}
+
+module "mysql" {
+  source                     = "./vendor/modules/mysql"
+  ENV                       = var.ENV
+  WORKSTATION_IP  =var.WORKSTATION_IP
+}
+
+module "rabbitmq" {
+  source                     = "./vendor/modules/rabbitmq"
   ENV                       = var.ENV
 }
