3f0b5f777a382e00ad875e808c143c24fb31f91d
priyankapeelu <priyankapeelu@icloud.com>
2022-05-09 15:49:05 +0530

    terraform variablescd tt

--- main.tf
+++ main.tf
@@ -1,15 +1,21 @@
-module "vpc" {
-  source = "./vendor/modules/vpc"
-  VPC_CIDR = var.VPC_CIDR
-  ENV = var.ENV
-  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  PUBLIC_SUBNET_CIDR =var.PUBLIC_SUBNET_CIDR
-  AZ =var.AZ
-  DEFAULT_VPC_ID = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT = var.DEFAULT_VPC_RT
-  HOSTEDZONE_PRIVATE_ID = var.HOSTEDZONE_PRIVATE_ID
-  HOSTEDZONE_PUBLIC_ID = var.HOSTEDZONE_PUBLIC_ID
-  HOSTEDZONE_PUBLIC_ZONE = var.HOSTEDZONE_PUBLIC_ZONE
+module "mongodb" {
+  source         = "./vendor/modules/mongodb"
+  ENV            = var.ENV
+  WORKSTATION_IP = var.WORKSTATION_IP
+}
 
+module "redis" {
+  source = "./vendor/modules/redis"
+  ENV    = var.ENV
+}
+
+module "mysql" {
+  source         = "./vendor/modules/mysql"
+  ENV            = var.ENV
+  WORKSTATION_IP = var.WORKSTATION_IP
+}
+
+module "rabbitmq" {
+  source = "./vendor/modules/rabbitmq"
+  ENV    = var.ENV
 }
